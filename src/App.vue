1d3c3ec54093c05f8eac11725153b1a01ea5fc04
Dereckson Santana <dereckson.santana@frotcom.com>
2018-10-15 12:21:51 +0100

    update project with Front-end test

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <section id="app">
+    <section class="col-lg-12">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+              <img class="logo-frotcom" alt="Frotcom International Logo" src="https://frotcomcdn.blob.core.windows.net/application-content/images/f-logo.png">
+            </a>
+          </div>
+          <ul class="nav nav-pills menu">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/vehicles">Vehicles</router-link></li>
+          </ul>
+
+          <ul class="nav nav-pills menu pull-right">
+            <li class="pull-right"><router-link to="/info">Info</router-link></li>
+          </ul>
+        </div>
+      </nav>
+    </section>
+    <router-view></router-view>
+    <section class="footer">
+      <section class="row">
+        <section class="container">
+          <h6>Frotcom International 2018</h6>
+        </section>
+      </section>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+.menu {
+  margin: 12px 0 8px;
+  display: block;
+  float: left;
+}
+.menu li a{
+  padding: 5px 10px;
+}
+.logo-frotcom {
+  width: 30px;
+  height: auto;
+  display: block;
+}
+
+.footer h6 {
+  text-align: center;
+}
+</style>
---
+++ src/pages/Vehicles.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="container">
+    <section class="row">
+      <section class="col-xs-12 col-md-12 col-lg-12">
+        <section class="panel panel-default">
+          <section class="panel-heading">
+              <h1>Vehicles</h1>
+          </section>
+          <section class="panel-body">
+            <h3>Render vehicle table</h3>
+          </section>
+        </section>
+      </section>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      vehicleData: '',
+    };
+  },
+  mounted()
+  {
+    console.log('mounted!!');
+  }
+};
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
